1834fada2c54215b4d4878ce236e83b82457b04d
Geedi Nikhil <geedinikhil24@gmail.com>
2026-01-05 00:19:14 +0530

    Remove ATS info side card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import UploadCard from "./components/UploadCard";
-import RightPanel from "./components/RightPanel";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-layout">
-      {/* Main Resume Analyzer Card */}
       <UploadCard />
-
-      {/* Ambient ATS Info Panel (hidden on mobile) */}
-      <RightPanel />
     </div>
   );
 }
